0f3095a839d8bc3a0a77f9e9c8ed165b2ce07737
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-04-27 18:41:47 -0400

    Criei os componentes header e navbar

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../navBar/NavBar"
+
+const Header = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+  <nav>
+    <ul>
+        <li>Início</li>
+        <li>Em alta</li>
+        <li>Os mais comprados</li>
+        <li>Promoções</li>
+    </ul>
+  </nav>
+  )
+}
+
+export default NavBar
