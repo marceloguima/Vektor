34c1f63236cf1767f7ded0a641932ba5038be412
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-05-01 09:26:42 -0400

    adicionei arquivos de imagens e apliquei algumas no hero

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Header />
-    </>
+    <div className="container">
+      <Header />
+    </div>
+    
   )
 }
 
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,14 +5,20 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="carolsel">
+        <div className="title">
         <h1>Lorem, ipsum dolor.</h1>
         <p>Lorem ipsum dolor sit amet.</p>
-        <img src="" alt="" />
+        </div>
+        <img src="./images/model-camis-azul.png" alt="modelo com camiseta polo azul" />
       </div>
+
       <div className="desc">
-        <h2>Lorem ipsum dolor sit amet.</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+        <div className="text-descr">
+          <h2>Lorem ipsum dolor sit amet.</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
         <button>Ver mais</button>
+        </div>
+        <img src="./images/sapato-preto3.png" alt="" />
       </div>
       <div className="cartao-fidelidade">
         <h2>Cartão fidelidade</h2>
